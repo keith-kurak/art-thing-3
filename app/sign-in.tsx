0d9b876dba748f8f002b758a84a8c93ca5c4bd74
Keith Kurak <keith.kurak@gmail.com>
2024-10-29 16:32:37 -0400

    fix logout on mobile

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { View, Text, TextInput, Platform } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Platform,
+  KeyboardAvoidingView,
+} from "react-native";
 import { useAuth } from "@/data/hooks/useAuth";
 import { useRouter } from "expo-router";
 import { Button } from "@/components/Button";
@@ -13,27 +19,29 @@ export default function LoginScreen() {
   const router = useRouter();
 
   return (
-    <View className="flex-1 justify-center items-center gap-y-6 bg-shade-0">
-      <TextField
-        label="Email"
-        text={email}
-        setText={setEmail}
-        autofocus={Platform.OS === "web"}
-      />
-      <TextField
-        label="Password"
-        text={password}
-        setText={setPassword}
-        isSecure
-      />
-      <Button
-        onPress={async () => {
-          await login(email, password);
-          router.replace("/(app)");
-        }}
-        label="Sign in"
-      />
-    </View>
+    <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
+      <View className="flex-1 justify-center items-center gap-y-6 bg-shade-0">
+        <TextField
+          label="Email"
+          text={email}
+          setText={setEmail}
+          autofocus={Platform.OS === "web"}
+        />
+        <TextField
+          label="Password"
+          text={password}
+          setText={setPassword}
+          isSecure
+        />
+        <Button
+          onPress={async () => {
+            await login(email, password);
+            router.replace("/(app)");
+          }}
+          label="Sign in"
+        />
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
