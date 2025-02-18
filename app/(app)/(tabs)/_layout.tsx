e421e4327c12ff3481734cbe8e9e0239d209ec88
Keith Kurak <keith.kurak@gmail.com>
2025-02-18 07:56:13 -0500

    fix tabs

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -18,7 +18,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="index"
+        name="(home)"
         options={{
           title: "Home",
           tabBarActiveTintColor: customColors.tint,
@@ -28,7 +28,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="exhibits"
+        name="(exhibits)"
         options={{
           title: "Exhibits",
           tabBarIcon: ({ color }) => (
